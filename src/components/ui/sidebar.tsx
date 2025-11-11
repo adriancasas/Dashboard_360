5cdecacb56c165759b225d3b6521d8660a8840aa
Adrián Casas <adriancasasizquierdo@gmail.com>
2025-11-11 11:06:17 +0000

    No, el cambio ha ido a peor. Ahora desplegado queda un hueco blanco enor

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -325,7 +325,7 @@ const SidebarInset = React.forwardRef<
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-200 ease-linear",
         "md:peer-data-[state=expanded]:ml-[--sidebar-width]",
-        "md:peer-data-[collapsible=icon]:peer-data-[state=collapsed]:ml-[--sidebar-width-icon]",
+        "md:peer-data-[collapsible=icon][data-state=collapsed]:ml-[--sidebar-width-icon]",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -764,5 +764,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
-    
